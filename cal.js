a16b7c473a892df41cab2dee7707c448853012a4
JungHaMin0 <gkals1703@naver.com>
2022-08-25 09:39:14 +0900

    @feat: 달려 기능 전체 완성 need: 전체적인 디자인, 31일 이후 공백, todolist 기능 추가

--- cal.js
+++ cal.js
@@ -1,21 +1,17 @@
-const calendar = () => {
-  const date = new Date();
-  const CurrentYear = date.getFullYear();
-  const CurrentMonth = date.getMonth() + 1;
-  const today = date.getDate();
+const calendar = (date) => {
+  const CurrentYear = new Date(date).getFullYear();
+  const CurrentMonth = new Date(date).getMonth() + 1;
+  const today = new Date(date).getDate();
   const prevMon = new Date(CurrentYear, CurrentMonth-1, 0).getDate();
-  //전 달의 마지막 날짜
   const thisMon = new Date(CurrentYear, CurrentMonth, 0).getDate();
-  //이번 달의 마지막 날짜
-
   const prevDay = new Date(CurrentYear, CurrentMonth-1, 1).getDay();
 
-  console.log(CurrentYear, CurrentMonth, today, prevMon, thisMon);
-  //2022 8 23 31 31
-  console.log(prevDay); //1
+  console.log(CurrentYear, CurrentMonth, today, prevMon, thisMon, prevDay);
+  //2022 8 23 31 31 1
 
   const dateBoard = document.querySelector('.dateBoard');
-// 다음달 날짜 표시하기
+  dateBoard.innerHTML = '';
+  // 다음달 날짜 표시하기
   for(let i=0; i<prevDay; i++){
     dateBoard.innerHTML += `<div class=""noColor">공백</div>`;
   }
@@ -24,12 +20,12 @@ const calendar = () => {
     dateBoard.innerHTML += "<div>" + i + "</div>";
   }
 
-  const dateTitle = document.querySelector('.dateTitle');
-  dateTitle.innerText = CurrentYear + "년 " + CurrentMonth + "월";
+  document.querySelector('.dateTitle').innerText = CurrentYear + "년 " + CurrentMonth + "월";
 
 };
 $(document).ready(calendar);
 
+//버튼 클릭 -> 날짜 변경
 const makeCal = () => {
   const date = new Date();
 
