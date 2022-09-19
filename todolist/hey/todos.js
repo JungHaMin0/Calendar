326e383f579d6c301a6a2976baf31a8c4a1866ff
JungHaMin0 <gkals1703@naver.com>
2022-09-19 16:02:57 +0900

    @feat: 체크박스 클릭시 빗금기능 추가
    need: 전반적인 퀄리티 높이기

--- todolist/hey/todos.js
+++ todolist/hey/todos.js
@@ -8,14 +8,25 @@ const todos = () => {
 }
 
 const paintTodos = (newTodo) => {
+  //const checkbox = document.querySelector("input[name=checkbox]");
+  //이러한 방법도 있다.
   const li = document.createElement("li");
+  const chkBox = document.createElement("input");
   const span = document.createElement("span");
   const button = document.createElement("button");
+  chkBox.type = "checkbox";
+  chkBox.id = "chking"
+  li.appendChild(chkBox);
   li.appendChild(span);
   li.appendChild(button);
   span.innerText = newTodo;
   button.innerText = "X";
   button.addEventListener("click", deleteTodos);
+  chkBox.addEventListener("change", function cheking(e) {
+    if(e.target.checked){
+      span.style.textDecoration = 'line-through';
+    }
+  });
   todoList.appendChild(li);
   alldel.addEventListener("click", function allDeleteTodos() {
     li.remove();
@@ -27,6 +38,16 @@ const paintTodos = (newTodo) => {
   });
 };
 
+/*
+const checking = (e) => {
+  if(e.target.checked){
+    console.log("체체킹");
+    
+    //text-decoration:line-through
+  }
+};
+*/
+
 const handleSubmit = (event) => {
   event.preventDefault();
   const newTodo = todoInput.value;
@@ -37,6 +58,6 @@ const handleSubmit = (event) => {
 const deleteTodos = (event) => {
   const removingOne = event.target.parentElement;
   removingOne.remove();
-}
+};
 
 todos();
